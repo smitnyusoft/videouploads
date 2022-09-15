fb82a9322697cd6aef97b43f2639f4e6027a0305
NS28 <smit.nyusoft@gmail.com>
2022-09-15 18:58:44 +0530

    firebase -changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player'
+import Button from '@mui/material/Button';
+import axios from 'axios';
 import './App.css';
 
+var URL = window.URL || window.webkitURL;
 function App() {
+  const [selectedFile, setSelectedFile] = useState('')
+  const [blobFile, setBlobFile] = useState('')
+  const [videoUrl, setVideoUrl] = useState('')
+
+  const onChangeHandler = (event) => {
+    var blobUrl = URL.createObjectURL(event.target.files[0]);
+    setVideoUrl(blobUrl)
+    setSelectedFile(event.target.files[0])
+  }
+
+  const uploadFileHandler = () => {
+    let formData = new FormData();
+    formData.append('file', selectedFile)
+    formData.append('blobVideo', videoUrl)
+    axios.post('http://localhost:4000/api/v1/upload-video/', formData)
+      .then((response) => {
+        console.log("response---->", response.data[0]);
+        setBlobFile(response.data[0].data.blobVideo)
+      }).catch((error) => {
+        console.log('error--->', error);
+      })
+  }
+
+  // useEffect(() => {
+  //   console.log("--- useEffect ---");
+  // }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPlayer
+        className='react-player'
+        width='50%'
+        height='50%'
+        //url={`http://localhost:4000/api/v1/upload-video/${videoUrl}`}
+        url={videoUrl}
+        playing={true}
+        controls={true}
+        //volume={5}
+        muted={true}
+      />
+      <label htmlFor="btn-upload">
+        <input
+          id="btn-upload"
+          name="btn-upload"
+          style={{ display: 'none' }}
+          type="file"
+          onChange={onChangeHandler}
+        />
+        <Button
+          style={{ marginBottom: '10px' }}
+          className="btn-choose"
+          variant="outlined"
+          component="span" >
+          Choose Files
+        </Button>
+      </label>
+      <div className="file-name">
+        {selectedFile && selectedFile.length > 0 ? selectedFile.name : null}
+      </div>
+      <Button
+        className="btn-upload"
+        color="primary"
+        variant="contained"
+        component="span"
+        disabled={!selectedFile}
+        onClick={uploadFileHandler}
+      >
+        Upload
+      </Button>
     </div>
   );
 }
